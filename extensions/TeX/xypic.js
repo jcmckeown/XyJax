9ef3b795edafba610b227d46f72282406dcefbbd
jcmckeown <qnoodles@gmail.com>
2017-06-17 13:07:07 -0400

    Update xypic.js

--- extensions/TeX/xypic.js
+++ extensions/TeX/xypic.js
@@ -16768,5 +16768,5 @@ MathJax.Hub.Register.StartupHook("SVG Xy-pic Require",function () {
   
   MathJax.Hub.Startup.signal.Post("SVG Xy-pic Ready");
 });
-
-MathJax.Ajax.loadComplete("[MathJax]/extensions/TeX/xypic.js");
+// https://cdn.rawgit.com/jcmckeown/XyJax/fixpoint/extensions/TeX/xypic.js
+MathJax.Ajax.loadComplete("https://cdn.rawgit.com/jcmckeown/XyJax/v0.0/extensions/TeX/xypic.js");
